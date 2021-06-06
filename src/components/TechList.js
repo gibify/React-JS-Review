7fb421c9a72794cc5792db793d6e582dc1161917
¨gibifyofficialgit config --global ussr.email ¨evandrogibicoski@outlook.com <evandrogibicoski@outlook.com>
2021-06-06 10:25:14 -0300

    feat: create TechList components

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -2,21 +2,41 @@ import React, { Component } from 'react';
 
 class TechList extends Component {
     state = {
+        newTech: '',
         techs: [
             'Node',
             'React',
             'React Native'
         ],
     };
+    // Sempre escrever a funcão no formato de Array function dentro das classes no React-JS
+    handleInput = e => {
+        this.setState({ newTech: e.target.value });
+    }
+
+    handleSubmit = e => {
+        e.preventDefault();
+        this.setState({ 
+            techs: [...this.state.techs, this.state.newTech],
+            newTech: ''
+        });
+    }
 
     render() {
-        console.log(this.state)
         return (
-            <ul>
-                <li>Node</li>
-                <li>React</li>
-                <li>React Native</li>
-            </ul>
+            <form onSubmit={this.handleSubmit}>
+                <ul>
+                  {this.state.techs.map(tech => (
+                      <li key={tech}>{tech}</li>
+                  ))}
+                </ul>
+                <input 
+                    type="text" 
+                    onChange={this.handleInput}
+                    value={this.state.newTech}
+                />
+                <button type="submit" >Adicionar</button>
+            </form>
         )
     }
 }
